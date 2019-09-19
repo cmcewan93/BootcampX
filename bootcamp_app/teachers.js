371fa89d4b12c0ce796385fc1c1db6afad08fd51
Colin McEWan <cmcewan93@gmail.com>
2019-09-19 12:47:53 -0400

    implement node-postgres

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+queryParams = process.argv.slice(2);
+
+const pool = new Pool({
+  user: 'colinmcewan',
+  password: 'test',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teachers.name;
+`)
+.then(res =>{
+  res.rows.forEach((teach) => {
+    console.log(`${teach.cohort}: ${teach.teacher}`)
+  })
+}).catch(err =>{
+  console.log(err);
+})
